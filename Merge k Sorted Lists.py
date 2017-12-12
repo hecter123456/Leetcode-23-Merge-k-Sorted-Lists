1aa7b529091bad292270d8665862696196511e7f
hecter_wu <hecter_wu@transcend.com.tw>
2017-12-12 16:42:53 +0800

    pass testcase: testEmptyList by extend non empty list

--- Merge k Sorted Lists.py
+++ Merge k Sorted Lists.py
@@ -5,12 +5,18 @@ class unitest(unittest.TestCase):
         Input = []
         Ans = []
         self.assertEqual(Solution().mergeKLists(Input),Ans);
+    def testEmptyList(self):
+        Input = [[],[1,2],[]]
+        Ans = [1,2]
+        self.assertEqual(Solution().mergeKLists(Input),Ans);
 
 class Solution():
     def mergeKLists(self, lists):
-        if lists == []:
-            return []
-        return None
+        Ans = []
+        for item in lists:
+            if item != []:
+                Ans.extend(item)
+        return Ans
 
 if __name__ == '__main__':
     unittest.main()
