84449944d3b6c813665fa8bfd2a9ec43dc51e0b6
hecter_wu <hecter_wu@transcend.com.tw>
2017-12-29 13:59:03 +0800

    Add testcase testSample

--- Merge k Sorted Lists.py
+++ Merge k Sorted Lists.py
@@ -2,12 +2,16 @@ import unittest
 
 class unitest(unittest.TestCase):
     def testNone(self):
-        Input = []
+        Input = [[]]
         Ans = []
         self.assertEqual(Solution().mergeKLists(Input),Ans);
     def testEmptyList(self):
         Input = [[],[1,2],[]]
         Ans = [1,2]
+        self.assertEqual(Solution().mergeKLists(Input),Ans);
+    def testSample(self):
+        Input = [[1,3],[1,2],[5]]
+        Ans = [1,1,2,3,5]
         self.assertEqual(Solution().mergeKLists(Input),Ans);
 
 class Solution():
@@ -16,6 +20,7 @@ class Solution():
         for item in lists:
             if item != []:
                 Ans.extend(item)
+        Ans.sort()
         return Ans
 
 if __name__ == '__main__':
